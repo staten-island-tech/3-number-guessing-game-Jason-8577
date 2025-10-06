b3d5fbb34ae41d583ea6142e358f089ee110c9bf
Jason-8577 <jasono119@nycstudents.net>
2025-10-06 06:21:38 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-print("guess the number from 1-10")
-number = input("Choose a number")
-for i in range(1, 1+1):
-    
+import random
+number=random.randint(1,10)
+print("guess a number from 1-10")
+Guess = input("Choose a number")
+while number == 
+
+""" def lang(x):
+    for char in x:
+        print(char)
+    lang("Cadee stole the capybara from Zi. Also Mason is the worst") """
+
