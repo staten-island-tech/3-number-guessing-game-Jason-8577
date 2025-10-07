7447e08c904f1f53c00c8edb9bb5d54f8537c1b2
Jason-8577 <jasono119@nycstudents.net>
2025-10-07 06:26:09 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,41 @@
 import random
-number=random.randint(1,10)
+number=random.randint(1, 10)
 print("guess a number from 1-10")
-Guess = input("Choose a number")
-while number == 
+for i in range(3):
+    guess = int(input("Choose a number"))
+    if guess < number:
+        print("Try again")
+    elif guess > number:
+        print("Try again")
+    else: 
+        print("choose a valid number")
+    guess = 4
+
+
+
+
+
+
+
+
+
+
+
 
 """ def lang(x):
+    t = 0
+    s = 0
     for char in x:
-        print(char)
-    lang("Cadee stole the capybara from Zi. Also Mason is the worst") """
+        if char == "t" or char =="T":
+            t+=1    
+        elif char == "s" or char =="S":
+            s+=1     
+        if t > s:
+            print("Eng")
+        else:
+            print("french")    
+    lang("Cadee stole the capybara from Zi. Also Mason is the worst")
 
+    def spaces(y,t):
+
+spaces("CCC..", ".C.C.C") """
